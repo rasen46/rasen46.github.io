90b14719e5b148a5569a76cddf2527cef19a96a5
rasen46 <136291290+rasen46@users.noreply.github.com>
2025-09-08 17:28:16 -0400

    Update script.js

--- utilities/script.js
+++ utilities/script.js
@@ -1,5 +1,6 @@
 // configs
 const jsonFiles = [
+  'v1.4_u2.json',
   'v1.4.json',
   'v1.3_u2.json',
   'v1.3.json',
@@ -115,6 +116,7 @@ function copyData(button) {
       alert("Failed to copy data!");
     });
 }
+
 
 
 
