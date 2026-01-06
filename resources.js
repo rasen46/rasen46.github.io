566c4de5c5fd376699d8d402fadc5235c011a360
rasen46 <136291290+rasen46@users.noreply.github.com>
2026-01-06 17:35:44 -0500

    add printing

--- resources.js
+++ resources.js
@@ -1,1 +1,15 @@
-
+function print(text, type) {
+  var cTime = getTime()-startTime;
+  var totalSeconds = Math.round(cTime / 1000);
+  
+  var formatToReadable =
+  ("0" + Math.floor(totalSeconds / 60)).slice(-2) + ":" +
+  ("0" + totalSeconds % 60).slice(-2) + ":" +
+  ("0" + Math.floor((cTime % 1000))).slice(-3);
+  
+  if (!type || typeof type != "string") {
+    console.log("["+formatToReadable+"] "+"[script.js/INFO]: "+text);
+  } else {
+    console.log("["+formatToReadable+"] "+"[script.js/"+type.toUpperCase()+"]: "+text);
+  }
+}
