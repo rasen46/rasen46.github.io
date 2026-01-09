1b5d9052ac028986c27c7dc17527150b3ad69d7d
rasen46 <136291290+rasen46@users.noreply.github.com>
2026-01-08 20:06:27 -0500

    Update resource links in rasenJSLib.js

--- jsLib/rasenJSLib.js
+++ jsLib/rasenJSLib.js
@@ -3,8 +3,8 @@ var rjs = {
 	modules: {},
 
 	// rasenJS resources v1.1.0 real //
-	// source file: https://github.com/rasen46/rasen46.github.io/blob/main/jsLib/rasenJSLib.js //
-	// example module loading: https://github.com/rasen46/rasen46.github.io/blob/main/jsLib/modules/example.js //
+	// source file: https://rasen46.github.io/jsLib/rasenJSLib.js //
+	// example module loading: https://rasen46.github.io/jsLib/modules/example.js //
 	print: function(text, type) {
 		var cTime = getTime() - rjs.startTime;
 
