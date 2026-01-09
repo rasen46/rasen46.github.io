7da98ed967c437097bcf9bae87402b7f60dae6fb
rasen46 <136291290+rasen46@users.noreply.github.com>
2026-01-08 19:45:02 -0500

    Update example.js

--- jsLib/modules/example.js
+++ jsLib/modules/example.js
@@ -1,3 +1,10 @@
+// to call modules via 'require' and 'loadmodule' its like this:
+//rjs.loadModule("https://api.github.com/repos/rasen46/rasen46.github.io/contents/jsLib/modules/example.js");
+//rjs.require("example.js", function (example) {
+//  if (!example) return;
+//  example.example();
+//});
+
 (function () {
   var module = {
     example: function () {
