24027dde4a1e37d8aada78900ea9b6c26b6a59b3
rasen46 <136291290+rasen46@users.noreply.github.com>
2026-01-08 20:02:19 -0500

    Update example.js

--- jsLib/modules/example.js
+++ jsLib/modules/example.js
@@ -1,4 +1,6 @@
-// to call modules via 'require' and 'loadmodule' its like this:
+// to call modules via 'require' and 'loadmodule' you first load the module, 
+// then require it (it is recommended to store the 'example' as a variable, which allows for better access of that required module):
+
 //rjs.loadModule("https://api.github.com/repos/rasen46/rasen46.github.io/contents/jsLib/modules/example.js");
 //rjs.require("example.js", function (example) {
 //  if (!example) return;
