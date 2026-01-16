4c50277f253bd47fcf77a038b41ce07f5dd3c51c
rasen46 <136291290+rasen46@users.noreply.github.com>
2026-01-15 19:52:20 -0500

    modified:   buh/drawing/main.html modified:   buh/drawing/script.js modified:   buh/drawing/style.css

--- buh/drawing/script.js
+++ buh/drawing/script.js
@@ -1,7 +1,12 @@
 const plrTextInp = document.getElementById("plrTextInput");
 const playerChatLog = document.getElementById("playerChat");
+const canvas = document.getElementById("canvas");
+
+const ctx = canvas.getContext("2d");
 
 const chatLog = [];
+
+let isDrawing = false, insideCanvas = false;
 
 plrTextInp.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
@@ -24,3 +29,44 @@ function updateChat() {
         playerChatLog.appendChild(msgDiv);
     });
 }
+
+const startDrawing = (event) => {
+    isDrawing = true;
+
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = "black";
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+
+    ctx.beginPath();
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+const draw = (event) => {
+    if (!isDrawing) return;
+
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+
+const updateSize = () => {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+}
+
+canvas.addEventListener("mouseleave", () => {
+    isDrawing = false;
+});
+
+canvas.addEventListener("mouseenter", (event) => {
+    if (event.buttons === 1) {
+        startDrawing(event);
+    }
+});
+
+canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", draw);
+
+window.addEventListener("resize", updateSize);
+updateSize();
